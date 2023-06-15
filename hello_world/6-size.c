619b9a1200a35b575f9bdd3cc363a3ab7d118674
root <agustinaaraujo1412@gmail.com>
2023-06-14 21:45:45 -0700

    holis

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int
+main(void)
+{
+	printf("Size of a char: %lu", sizeof(char));
+	printf(" byte(s)\n")
+	printf("size of an int: %lu", sizeof(int));
+	printf(" byte(s)\n");
+	printf("size of a long int: %lu", sizeof(long int));
+	printf(" byte(s)\n");	
+	printf("size of a long long int: %lu", sizeof(long long int));
+        printf(" byte(s)\n");
+	printf("size of a float: %lu", sizeof(float));
+	printf(" byte(s)\n");
+	return (0);        	
+}
