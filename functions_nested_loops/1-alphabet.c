239b8304abd8ad339feb0cb571222c1756caa9dc
root <agustinaaraujo1412@gmail.com>
2023-06-16 08:39:22 -0700

    holis

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
-#include <ctype.h>
+#include "main.h"
 
 void
 print_alphabet(void)
 {
 char c;
-for (c = 'a'; c <= 'z'; ++c)
+for (c = 'a'; c <= 'z'; c++)
+{
 _putchar(c);
+}
 _putchar('\n');
 return (0);
 }
