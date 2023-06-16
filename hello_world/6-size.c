ed3d3e981ef5499c0a58782ebb4424dd002b8380
root <agustinaaraujo1412@gmail.com>
2023-06-15 17:12:32 -0700

    holis

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,7 +8,7 @@ printf(" byte(s)\n");
 printf("size of an int: %lu", sizeof(int));
 printf(" byte(s)\n");
 printf("size of a long int: %lu", sizeof(long int));
-printf(" byte(s)\n");	
+printf(" byte(s)\n");
 printf("size of a long long int: %lu", sizeof(long long int));
 printf(" byte(s)\n");
 printf("size of a float: %lu", sizeof(float));
