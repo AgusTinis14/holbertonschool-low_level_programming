cd00b1d78e7f89f4227d38285cd598a4f28d4a97
root <agustinaaraujo1412@gmail.com>
2023-06-15 19:50:07 -0700

    holis

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int
+main(void)
+{
+char c;
+while (c != 'g')
+{
+if (c <= '9')
+{
+for (c = '0'; c < '9'; ++c)
+putchar(c);
+}
+else
+{
+c = 'a';
+while (c != 'g')
+{
+putchar(c);
+++c;
+}
+}
+}
+puchar('\n');
+return (0);
+}
