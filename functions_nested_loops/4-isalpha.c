aab9a34e687d4dff054916a863bb447500156420
root <agustinaaraujo1412@gmail.com>
2023-06-16 18:14:12 -0700

    holis

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "stdio.h"
 
 int
-_isalpha(int c);
+_isalpha(int c)
 {
 int r;
-if (c >= 96 && C <= 123)
+if (c >= 96 && c <= 123)
 r = 1;
-else if (c >= 64 && c <=91)
-r =1;
+else if (c >= 64 && c <= 91)
+r = 1;
 else
 r = 0;
 return (r);
