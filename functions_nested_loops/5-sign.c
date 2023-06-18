43f828af23d413627b0e869c48953aef592acf4e
root <agustinaaraujo1412@gmail.com>
2023-06-18 03:08:42 -0700

    holis

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,1 +1,23 @@
-#include <stdio.h>
+#include "main.h"
+
+int
+print_sign(int n)
+{
+int r;
+if (n == 0)
+{
+r = 0;
+_putchar('0');
+}
+if (n > 0)
+{
+r = 1;
+_putchar('+');
+}
+if (n < 0)
+{
+r = -1;
+_putchar('-');
+}
+return (r);
+}
