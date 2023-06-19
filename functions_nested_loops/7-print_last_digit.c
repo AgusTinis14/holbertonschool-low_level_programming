8141012a5c8e4837fbb8059afbadf5a276b6b4c8
root <agustinaaraujo1412@gmail.com>
2023-06-19 09:43:47 -0700

    holis

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
-i#include "main.h"
+#include "main.h"
+#include <stdio.h>
 
 int
 print_last_digit(int n)
@@ -8,6 +9,6 @@ if (last_digit < 0)
 {
 last_digit * = -1;
 }
-_putchar(last_digit + '0');
+_putchar (last_digit + '0');
 return (last_digit);
 }
