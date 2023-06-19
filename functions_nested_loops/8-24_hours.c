a10b9dc426bce6352d34ae3fa450393ac1aaa16e
root <agustinaaraujo1412@gmail.com>
2023-06-19 10:38:44 -0700

    holis

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void jack_bauer(void)
+{
+int hour, minute;
+for (hour = 0; hour < 24; hour++)
+{
+for (minute = 0; minute < 60; minute++)
+{
+_putchar("%02:%02\n", hour, minute);
+}
+}
+}
