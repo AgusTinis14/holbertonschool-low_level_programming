502e7cb68b8555e4b65f5403b24dfcfed940b741
root <agustinaaraujo1412@gmail.com>
2023-06-19 11:18:34 -0700

    holis

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ * jack_bauer - print the watch of jack_bauer
+ *
+ */
 
-void
-jack_bauer(void)
+void jack_bauer(void)
 {
 int hour, minute;
 for (hour = 0; hour < 24; hour++)
@@ -11,7 +14,7 @@ for (minute = 0; minute < 60; minute++)
 {
 _putchar((hour / 10) + '0');
 _putchar((hour % 10) + '0');
-_putchar(':')
+_putchar(':');
 _putchar((minute / 10) + '0');
 _putchar((minute % 10) + '0');
 _putchar('\n');
