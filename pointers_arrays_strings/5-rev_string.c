b6e74f287c014d76db0fff257b00aabed004e74b
root <agustinaaraujo1412@gmail.com>
2023-06-23 10:34:03 -0700

    holis

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+void
+rev_string(char *s)
+{
+int n;
+int a;
+char mario;
+
+n = 0
+for (n = 0; s[a]; a++)
+{
+	continue;
+}
+n -= 1;
+for (; s[a]; n--)
+{
+	mario[a] = s[n];
+	a++;
+}
+for (n = 0; s[n]; n++)
+	s[a] = mario[a];
+}
