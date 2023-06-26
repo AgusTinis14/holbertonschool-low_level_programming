000e4479671f5ef3fdce63b4a68617bde80faeac
root <agustinaaraujo1412@gmail.com>
2023-06-26 09:08:03 -0700

    holis

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -6,13 +6,13 @@ char
 {
 	int b, a;
 
-	for (b = 0; dest[b] != NULL; b++)
+	for (b = 0; dest[b] != '\0'; b++)
 		;
-	for (a = 0; src[a] != NULL; a++)
+	for (a = 0; src[a] != '\0'; a++)
 	{
 		dest[b] = src[a];
-		b++
+		b++;
 	}
-	dest[b] = NULL;
+	dest[b] = '\0';
 	return (dest);
 }
