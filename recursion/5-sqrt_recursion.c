1e2e705cad9c94dd6bb03ed1978d98d687f829b3
root <agustinaaraujo1412@gmail.com>
2023-06-29 07:15:29 -0700

    recursion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -5,7 +5,7 @@
  *_sqrt_recursion - return the natural square root of a number
  *@n: int number
  *return: if no natural square root, return -1. Else return natural
- square root
+ *square root
  */
 
 int _sqrt_recursion(int n)
