41a209487036ac4e946f65e6b12f35351f0ee49e
root <agustinaaraujo1412@gmail.com>
2023-06-29 09:27:17 -0700

    recursion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,26 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 
-/**
- *_sqrt_recursion - return the natural square root of a number
- *@n: int number
- *return: if no natural square root, return -1. Else return natural
- *square root
- */
-
-int _sqrt_recursion(int n)
+int
+_sqrt_recursion(int n)
 {
 	return (halp(n, 1));
 }
 
-/**
- *halp - helper function to solve _sqrt_recursion
- *@c: number to determine if square root
- *@i: increment to compare against 'c'
- *Return: square root if natural square root, or -1 if none found
- */
-
-int halp(int c, int i)
+int
+halp(int c, int i)
 {
 	int square;
 
