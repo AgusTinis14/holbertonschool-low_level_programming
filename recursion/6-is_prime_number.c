f933ba184853db90c8769cd4a3d7177c80624ae1
root <agustinaaraujo1412@gmail.com>
2023-06-29 10:17:05 -0700

    recursion

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -4,8 +4,16 @@
 int
 is_prime_number(int n)
 {
-	if (n % 3 != 0 || n % 5 != 0 || n = 2)
-		return (1);
-	else
+	if (n < 0)
+		n = n * -1;
+	if (n == 1)
 		return (0);
+	if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0)
+	{
+		if (n == 2)
+			return (1);
+		return (0);
+	}
+	else
+		return (1);
 }
