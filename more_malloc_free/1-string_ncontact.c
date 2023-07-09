176b0234b37c7b83596d714a1dd3dd1d69575ed9
root <agustinaaraujo1412@gmail.com>
2023-07-08 22:27:16 -0700

    holis

--- more_malloc_free/1-string_ncontact.c
+++ more_malloc_free/1-string_ncontact.c
@@ -1,8 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 
-char
-*string_nconcat(char *s1, char *s2, unsigned int n)
+/**
+ * string_nconcat - concatenate 2 strings, only n bytes of s2
+ * @s1: string 1
+ * @s2: string 2
+ * @n: bytes to include of s2
+ * Return: NULL if fail, else pointer to malloc memory
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
 	int strlen1, i, c;
