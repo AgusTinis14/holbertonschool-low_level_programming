22bad4e1e35efb9b6453b8597be07e837e8e0731
root <agustinaaraujo1412@gmail.com>
2023-07-11 08:06:45 -0700

    holis

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,17 +1,13 @@
+#ifndef DOG_H
+#define DOG_H
 #include <stdio.h>
-#include "dog.h"
-#ifndef DOG.H
-#define DOG.H
 
-typedef struct dog
+typedef
+struct dog
 {
 	char *name
 	float age
 	char *owner
-} dog_t;
+}
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 #endif
