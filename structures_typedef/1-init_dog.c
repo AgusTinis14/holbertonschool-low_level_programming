45047d5d3abe682dd6235dc7d121b5b5c151fac4
root <agustinaaraujo1412@gmail.com>
2023-07-11 07:36:58 -0700

    holis

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "dog.h"
+#include "dog.h"
+#include <stdlib.h>
 
 void
 init_dog(struct dog *d, char *name, float age, char *owner)
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include "dog.h"
+#ifndef DOG.H
+#define DOG.H
 
 typedef struct dog
 {
