4104177e5861b15ee68b2d8fc11ae7d3a5d68e30
root <agustinaaraujo1412@gmail.com>
2023-07-12 15:22:37 -0700

    holis

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,15 +1,8 @@
-#include <stdio.h>
 #include "dog.h"
 #include <stdlib.h>
 
-/**
- * init_dog - initialize a variable of type `struct dog`
- * @d: struct dog
- * @name: string for name
- * @age: integer for age
- * @owner: string for owners name
- */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+void
+init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,14 +1,16 @@
-#ifndef DOG_H
-#define DOG_H
-#include <stdio.h>
+#ifndef dog_h
+#define dog_h
 
-typedef
-struct
-dog
+/**
+ * struct dog - structure definition of a dog
+ * @name: character string
+ * @age: integer
+ * @owner: character string
+ */
+
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
-
-#endif
+}
