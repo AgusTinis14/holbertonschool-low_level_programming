dd0592a0ec9ec372d8562c272929ac1cf98aaf0d
root <agustinaaraujo1412@gmail.com>
2023-07-12 15:00:19 -0700

    holis

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,15 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 #include <stdio.h>
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-typedef struct dog
+
+typedef
+struct
+dog
 {
 	char *name;
 	float age;
 	char *owner;
 } dog_t;
+
 #endif
