150bc04b569c37a4f81cc6fc07ca911c9b244f17
root <agustinaaraujo1412@gmail.com>
2023-07-13 09:25:27 -0700

    holis

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,8 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 #include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
@@ -9,6 +11,7 @@ void _printchar(va_list list);
 void _printstr(va_list list);
 void _printfloat(va_list list);
 void _printint(va_list list);
+int _putchar(char c);
 
 typedef struct checker
 {
