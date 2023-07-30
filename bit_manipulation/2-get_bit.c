d0d76bbf082b7bd7c91712daeab3764ecc23f558
root <agustinaaraujo1412@gmail.com>
2023-07-30 13:52:06 -0700

    bit manipulation

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* get_bit - The value of a bit at a given index
+* @n: Is a number
+* @index: is the index, starting form 0
+* Return: the value of the bit at index index or -1 if an error occured
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > 64)
+		return (-1);
+	return ((n >> index) & 1);
+}
