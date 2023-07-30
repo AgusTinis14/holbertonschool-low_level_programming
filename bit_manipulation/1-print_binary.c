27267f5c71cbc0100c8426ddfa64c1519cc59d8a
root <agustinaaraujo1412@gmail.com>
2023-07-30 13:56:50 -0700

    bit manipulation

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -1,25 +1,11 @@
 #include "main.h"
-#include <string.h>
 /**
-* binary_to_uint - Binary to Decimal number.
-* @b: is pointing to a string of 0 and 1 chars
-* Return: return the converted number
+* print_binary - That prints the binary representation of a number
+* @n: Is a number to convert to binary
 */
-unsigned int binary_to_uint(const char *b)
+void print_binary(unsigned long int n)
 {
-	int result = 0;
-	int i = 0, power = 1;
-
-	if (!b)
-		return (0);
-	i = strlen(b) - 1;
-	for (; i >= 0; i--)
-	{
-		if (b[i] == '1')
-			result += power;
-		else if (b[i] != '0')
-			return (0);
-		power *= 2;
-	}
-	return (result);
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
